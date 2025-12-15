ff08a8add462ee5db3a4052e5b9b9e9643429b80
Grant Hayes <228530441+granthayes2006-code@users.noreply.github.com>
2025-12-14 19:43:06 -0600

    Commit test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/awd1000-final-project/'
+  base: '/awd1000-final-project/',
+  plugins: [react()]
 })
